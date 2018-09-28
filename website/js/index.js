515d1e5dbdc57d975686d36151ac9c36175481b4
almahdiy <yaqeen.a.almahdi@gmail.com>
2018-09-28 16:25:21 +0300

    website initial commit

---
+++ website/js/index.js
@@ -0,0 +1,10 @@
+function ExpandMenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+
