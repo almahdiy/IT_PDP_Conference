5e65a7097ac827ce635e91f580ea2de86c5c0321
Oz <oz.alikhan@gmail.com>
2018-10-27 10:07:10 +0300

    Fixed bug where clicking home on navbar doesn't redirect to home

--- website/webapp/urls.py
+++ website/webapp/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
-#from django.contrib import admin
-from . import views # import a file called views from the same directory that I am in
+from . import views     # Relative importing
 
 
+# Look for the url that matches the user's request
 urlpatterns = [
-    #look for the url that matches the user's request
-    url(r'^$', views.home, name="home"), #default homepage; they didn't request anything
+    url(r'^$|^home$', views.home, name="home"),     # Default homepage; they didn't request anything
+    url(r'^agenda$', views.agenda, name="agenda"),
     url(r'^agenda$', views.agenda, name="agenda"),
     url(r'^booths$', views.booths, name="booths"),
     url(r'^splash$', views.splash, name="splash"),
